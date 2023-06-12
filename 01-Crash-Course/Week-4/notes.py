a8e33813382686486b23df0a279d8852a5b7c300
Ntt37Vp <fuckspammers@protonmail.com>
2023-06-12 21:27:51 +0800

    Course 1 Week 4 - notes on Strings, Lists, Dicts

---
+++ 01-Crash-Course/Week-4/notes.py
@@ -0,0 +1,27 @@
+# Course 1 Week 4 - Strings, Lists & Dicts
+
+# Strings are Arrays
+# Assigning string to a var
+    # greeting = "Hello World"
+
+# Multi-line string using three quotes
+    '''
+    Lorem ipsum dolor sit amet,
+    consectetur adipiscing elit,
+    sed do eiusmod tempor incididunt
+    ut labore et dolore magna aliqua.
+    '''
+
+# String data type can be sliced, modified and concantenated
+# String Methods examples
+    # upper()
+    # lower()
+    # strip()
+
+# Sample function that replaces old domain by new domain
+def replace_domain(email, old_domain, new_domain):
+    if "@" + old_domain in email:
+        index = email.index("@" + old_domain)
+        new_email = email[:index] + "@" + new_domain
+        return new_email
+    return email
