1b0ac4a1a53ada965a0cb4164802e54a34bbeb07
Ntt37Vp <fuckspammers@protonmail.com>
2023-06-12 22:45:33 +0800

    more string methods

--- 01-Crash-Course/Week-4/notes.py
+++ 01-Crash-Course/Week-4/notes.py
@@ -19,9 +19,10 @@
     # strip()
 
 # Sample function that replaces old domain by new domain
-def replace_domain(email, old_domain, new_domain):
-    if "@" + old_domain in email:
-        index = email.index("@" + old_domain)
-        new_email = email[:index] + "@" + new_domain
-        return new_email
-    return email
+# def replace_domain(email, old_domain, new_domain):
+#     if "@" + old_domain in email:
+#         index = email.index("@" + old_domain)
+#         new_email = email[:index] + "@" + new_domain
+#         return new_email
+#     return email
+
