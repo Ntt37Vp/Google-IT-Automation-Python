82d7da08eb168985413de7e2a6d311036510f393
Ntt37Vp <fuckspammers@protonmail.com>
2023-06-17 18:33:51 -0400

    Course 1 Week 4 - added study guide for the graded assessment

---
+++ 01-Crash-Course/Week-4/prac-quiz-2.py
@@ -0,0 +1,19 @@
+given_group_dict = {
+    "local": ["admin", "userA"],
+    "public": ["admin", "userB"],
+    "administrator": ["admin"]
+}
+
+
+def groups_per_user(group_dictionary):
+    user_groups = {}
+    for group, users in group_dictionary.items():
+        for user in users:
+            if user in user_groups:
+                user_groups[user].append(group)
+            else:
+                user_groups[user] = [group]
+    return user_groups
+
+
+print(groups_per_user(given_group_dict))
--- 01-Crash-Course/Week-4/practice-quiz.py
+++ 01-Crash-Course/Week-4/practice-quiz.py
@@ -136,3 +136,51 @@
 # Pat is 35 years old and works as Lawyer
 # Amanda is 25 years old and works as Engineer
 # """
+
+##### PRACTICE QUIZ: DICT #####
+# 1 receive a dict
+def email_list(domains):
+    emails = []
+    for domain, users in domains.items():
+        for user in users:
+            emails.append(user + '@' + domain)
+    return emails
+
+
+print(email_list({
+    "gmail.com": ["clark.kent", "diana.prince", "peter.parker"],
+    "yahoo.com": ["barbara.gordon", "jean.grey"],
+    "hotmail.com": ["bruce.wayne"]
+}))
+
+# 2 - func receives a dict with group names with list of users.
+# users can belong to multi group
+
+
+# def groups_per_user(group_dictionary):
+#     user_groups = {}
+#     for group, users in group_dictionary.items():
+#         for user in users:
+#             if user in user_groups:
+#                 user_groups[user].append(group)
+#             else:
+#                 user_groups[user] = [group]
+#     return user_groups
+#
+#
+# print(groups_per_user({
+#     "local": ["admin", "userA"],
+#     "public": ["admin", "userB"],
+#     "administrator": ["admin"]
+# }))
+
+
+# 3
+# wardrobe = {'shirt': ['red', 'blue', 'white'], 'jeans': ['blue', 'black']}
+# new_items = {'jeans': ['white'], 'scarf': ['yellow'], 'socks': ['black', 'brown']}
+# wardrobe.update(new_items)
+
+
+# 4 -
+
+# 5 -
