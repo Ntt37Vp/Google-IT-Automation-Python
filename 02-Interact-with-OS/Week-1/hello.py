9be572e23d68f252623114c0865d38541cb08cc7
Ntt37Vp <fuckspammers@protonmail.com>
2023-06-20 01:18:54 +0800

    Course 2 Week 1 - init

---
+++ 02-Interact-with-OS/Week-1/hello.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+print("Hellooo World! \n" * 10)
+
---
+++ 02-Interact-with-OS/Week-1/notes.py
@@ -0,0 +1,13 @@
+# Course 2 - Using Python to Interact with the Operating System (Week 1)
+
+
+# Getting Ready
+    # PyPi
+    # pip
+
+# Use the SHEBANG line for your py scripts
+    #!/usr/bin/env python3
+
+# Update Permission to run
+    # chmod +x filename.py
+
