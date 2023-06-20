4522ddf2b24cb8b4c6b544eabb90ddc0ec5b80ff
Ntt37Vp <fuckspammers@protonmail.com>
2023-06-19 20:52:08 -0400

    Course 2 Week 1 - practical examples

--- 02-Interact-with-OS/Week-1/notes.py
+++ 02-Interact-with-OS/Week-1/notes.py
@@ -1,7 +1,7 @@
 # Course 2 - Using Python to Interact with the Operating System (Week 1)
 
 
-# Getting Ready
+# Getting System Ready
     # PyPi
     # pip
 
@@ -11,3 +11,18 @@
 # Update Permission to run
     # chmod +x filename.py
 
+
+### Example 1 - Disk Usage ###
+# import shutil
+#
+# du = shutil.disk_usage("/")
+# # print(du)
+#
+# usage = (du.free / du.total) * 100
+# print("Free disk is {:.2f}% ".format(usage))
+
+
+### Example 2 - CPU Usage ###
+import psutil
+
+print(psutil.cpu_percent(0.1))
