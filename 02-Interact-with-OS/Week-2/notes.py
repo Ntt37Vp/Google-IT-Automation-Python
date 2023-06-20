28ec0baf8c0bdcda8f3b18097ba78b21a690e7f0
Ntt37Vp <fuckspammers@protonmail.com>
2023-06-21 05:36:42 +0800

    interacting with files, os module,

--- 02-Interact-with-OS/Week-2/notes.py
+++ 02-Interact-with-OS/Week-2/notes.py
@@ -1,6 +1,6 @@
-### Course 2 Week 2 - Reading & Writing Files ###
+### COURSE 2 - Week 2 - Reading & Writing Files ###
 
-## Using open() and close()
+# Using open() and close()
 
 # file_var = open("file.txt")
 
@@ -9,9 +9,26 @@
 # file.close()
 
 
-## Using with() block instead 
-with open("file.txt") as file_var:
-	print(file_var.read())
+# Using with() block instead
+# with open("file.txt") as file_var:
+# 	print(file_var.read())
+# # no need to close() !!!! :)
 
-# no need to close() !!!! :)
 
+# Iterating through files
+# with open('file.txt') as file:
+# 	for line in file:
+# 		print(line.upper())
+
+
+# Writing Files (remember to use the W mode)
+# with open('file2.txt', "w") as file:
+#     file.write("It was a dark and stormy night")
+
+
+# USING OS module
+import os
+
+# os.remove('file2.txt')
+# os.rename('filename')
+# os.path.exists('file3.txt')
