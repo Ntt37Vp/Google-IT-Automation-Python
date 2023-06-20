7baca9cebea330f83eeb080dd172a12e9a4beb6f
Ntt37Vp <fuckspammers@protonmail.com>
2023-06-21 00:24:29 +0800

    Course 2 Week 2 - more notes

--- 02-Interact-with-OS/Week-2/notes.py
+++ 02-Interact-with-OS/Week-2/notes.py
@@ -1,4 +1,17 @@
 ### Course 2 Week 2 - Reading & Writing Files ###
 
-# file_var = open("filename")
+## Using open() and close()
+
+# file_var = open("file.txt")
+
+# print(file_var.readline())
+# print(file_var.read())
+# file.close()
+
+
+## Using with() block instead 
+with open("file.txt") as file_var:
+	print(file_var.read())
+
+# no need to close() !!!! :)
 
