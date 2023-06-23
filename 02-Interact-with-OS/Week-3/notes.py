d0527a942aca1c34df8b5c6849a4bb0b6eb56a67
Ntt37Vp <fuckspammers@protonmail.com>
2023-06-23 10:10:38 +0800

    regex

---
+++ 02-Interact-with-OS/Week-3/notes.py
@@ -0,0 +1,13 @@
+# Course 2 Week 3 - RegEx
+
+# RegEx 
+# A RegEx, or Regular Expression, is a sequence of characters that forms a search pattern.
+# RegEx can be used to check if a string contains the specified search pattern.
+
+# Using re module
+import re
+
+txt = "The rain in Spain [12345] que sera whatever"
+regex = r"\[(\d+)\]"
+result= re.search(regex, txt)
+print(result[1]) 
