7dad7dc5f3a034c7d06e85ff0e42b9b48a52ccfd
Ntt37Vp <fuckspammers@protonmail.com>
2023-06-26 22:38:12 +0800

    python subprocess

--- 02-Interact-with-OS/Week-4/notes.py
+++ 02-Interact-with-OS/Week-4/notes.py
@@ -16,7 +16,50 @@
 # /usr/sbin/temp is NOT listed by default
 
 # using the OS module
+# using SYS module
 import os
+import sys
+import subprocess
 
-print("Home: " + os.environ.get("HOME"))
+
+print("Home: " + os.environ.get("HOME", ""))
 print("SHELL: " + os.environ.get("SHELL", ""))
+print("FRUIT: " + os.environ.get("FRUIT", ""))
+
+# The .get method is quiet helpful
+
+# using the export command to save an ENV VAR
+# export FRUIT=Pineapple
+# when the script is ran again, the Banana env var will show
+
+
+# Command line arguments --  parameters that are passed to a program when it started.
+
+
+# print(sys.argv)
+#  The list of arguments are stored in the sys module.
+#  exit status is a value returned by a program to the shell
+
+#  to check the EXIT status using $?
+# echo $?
+
+# WC command prints out the number of lines, words and chars in a file
+# exit status codes
+# 0 - success
+# 1 - failed
+
+
+# In Python 3
+
+# Input handles string as a generic string. It does not evaluate the string as a string expression.
+
+# raw_input doesn’t exist, but with some tricky techniques, it can be supported.
+
+# eval() can be used the same as Python 2.
+
+
+# Python Subprocesses
+# import subprocess
+# use the .run method to run a linux system command
+# example, to run "date" command enter
+# subprocess.run(["date"])
