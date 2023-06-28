37a5910c5eb3f866246fa03b0352ce62a506b82b
Ntt37Vp <fuckspammers@protonmail.com>
2023-06-28 11:11:11 +0800

    week 5 - unit tests - init

---
+++ 02-Interact-with-OS/Week-5-Tests/notes.py
@@ -0,0 +1,15 @@
+# Course 2 - Week 5 - TESTS
+
+# Software testing is a process of evaluating computer code to determine whether or not it does what you expect it to do.
+
+# Simple Tests
+# Manual vs Automated Test
+# test cases
+
+# Unit Tests
+# the most common type, for small, isolated parts of code
+
+
+# Other Tests Concepts
+
+# Errors & Exceptions
