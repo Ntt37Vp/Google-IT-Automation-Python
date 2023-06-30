40d53712f6d619ed6a8eb5f4033f82749c65105d
Ntt37Vp <fuckspammers@protonmail.com>
2023-07-01 06:23:40 +0800

    added week 5 lab notes

--- 02-Interact-with-OS/Week-5-Tests/notes.py
+++ 02-Interact-with-OS/Week-5-Tests/notes.py
@@ -83,3 +83,16 @@
 #         testcase = "The best preparation for tomorrow is doing your best today."
 #         expected = ['b', 'a', 'a', 'b', 'a']
 #         self.assertEqual(LetterCompiler(testcase), expected)
+
+
+# Lab notes
+# Revised Guess() function
+# def Guess(participants):
+#     my_participant_dict = {}
+#     for participant in participants:
+#         my_participant_dict[participant] = random.randint(1, 9)
+#     try:
+#         if my_participant_dict['Larry'] == 9:
+#             return True
+#     except:
+#         return None
