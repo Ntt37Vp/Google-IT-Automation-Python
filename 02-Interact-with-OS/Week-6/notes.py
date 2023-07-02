c2997643cfbb502c81edcc80d40ad4e4f02f66df
Ntt37Vp <fuckspammers@protonmail.com>
2023-07-02 09:26:51 +0800

    Course 2 Week 6 - bash scripting - init

---
+++ 02-Interact-with-OS/Week-6/notes.py
@@ -0,0 +1,24 @@
+# Course 2 Week 6 - Command line
+
+# Interacting with command line
+# Basic
+
+# echo
+# cat
+# chmod
+# mkdir
+# cd
+# ls
+# pwd
+# cp
+# mv
+# . shortcut is the cwd
+# touch
+# permissions:
+# rm
+# rmdir (for empty dir)
+
+
+# Bash scripting
+
+# Advanced bash
