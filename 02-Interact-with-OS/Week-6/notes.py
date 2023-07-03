5243fef4cb5a9d67a384f46503825278dfeaae95
Ntt37Vp <fuckspammers@protonmail.com>
2023-07-03 12:37:17 +0800

    Course 2 Week 6 - more signal streams

--- 02-Interact-with-OS/Week-6/notes.py
+++ 02-Interact-with-OS/Week-6/notes.py
@@ -18,6 +18,27 @@
 # rm
 # rmdir (for empty dir)
 
+# redirecting streams
+# use > to redirect (overrides)
+# example below creates hello.txt with 'hello world!' as values
+# echo 'hello world!' > hello.txt
+
+# user >> redirect method to APPEND
+# example below appends new line to the previous file above
+# echo 'hello again, world!' >> hello.txt
+
+# Use 2> to redirect StdErr messages
+
+# Pipes and Pipelines aka piping
+# use |
+# example below, pipes the output of spider.txt to TR (translate) command that transforms the spaces ' ' , into nl \n
+# cat spider.txt | tr ' ' '\n'
+# tr command translate or delete char
+
+# SYS module has a usefull STDIN function
+# see attached haiku.txt and capitalize.py as examples
+# cat haiku.txt | ./capitalize.py
+
 
 # Bash scripting
 
