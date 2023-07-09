62b0733506c8f1042996ab4d43e59411f9036554
Ntt37Vp <fuckspammers@protonmail.com>
2023-07-09 15:21:31 -0400

    Week 1 - Git

--- 03-Git/Week-1/notes.py
+++ 03-Git/Week-1/notes.py
@@ -1,2 +1,19 @@
 # Course 3 - Git & Github
 # Week 1
+
+# Before VCS
+# DIFF 
+# example:
+# DIFF file1 file2
+# -u flag shows a unified format
+# DIFF -u file1 file2
+# wdiff shows the words difference
+# the DIFF output could also be redirected into a .diff file
+# format:
+# DIFF -u file1 file2 > change.diff
+
+# .diff can be manually worked on OR automate using PATCH command! 
+# to use the PATCH:
+# PATCH target_file < fix_file.diff
+
+# Here comes VCS
