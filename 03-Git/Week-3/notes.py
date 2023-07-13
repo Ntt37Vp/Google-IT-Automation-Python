abcf9ca729445b551936042c80e805930426cfc7
Ntt37Vp <fuckspammers@protonmail.com>
2023-07-13 21:58:49 +0800

    week 3 init

--- 03-Git/Week-3/notes.py
+++ 03-Git/Week-3/notes.py
@@ -1,1 +1,18 @@
 # Course 3 - Week 3 - Remote Repo (GitHub, etc)
+
+# git clone
+# git push
+
+# Using SSH (recommended)
+# git config --global user.name "Your Name"
+# git config --global user.email "yourname@example.com"
+# ls ~/.ssh/id_ed25519.pub
+# ssh-keygen -t ed25519 -C <youremail>
+# cat ~/.ssh/id_ed25519.pub
+# Upload .pub to GitHub settings
+
+# Using Cred Helper (15 mins only)
+# git config --global credential.helper cache
+
+
+# git pull
