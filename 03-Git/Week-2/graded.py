6045d18767bab740246541e1619e15cec85e3663
Ntt37Vp <fuckspammers@protonmail.com>
2023-07-17 19:58:30 +0800

    Finished Week 2 Graded; added Week 3 notes

---
+++ 03-Git/Week-2/graded.py
@@ -0,0 +1,21 @@
+# git branch improve-output
+# git checkout improve-output
+# nano food_count.py
+# make changes on food_count.py
+
+# git add food_count.py
+# git commit -m "Adding a line in the output describing the utility of food_count.py script"
+
+
+# Fix the food_question.py scipt
+# git log
+# git revert [commit-ID]
+# double check the script
+# ./food_question.py
+# MERGE!
+# git checkout master
+# git merge improve-output
+# Test the script again
+# ./food_question.py
+# git status
+# git log
--- 03-Git/Week-3/notes.py
+++ 03-Git/Week-3/notes.py
@@ -16,3 +16,28 @@
 
 
 # git pull
+
+# git remote show origin
+# git branch -r
+# git fetch
+# git merge
+# Note: git pull instantly merges while git fetch only retrieves remote updates.
+
+# Git Remote Summary
+# git remote        | Lists remote repos
+# git remote -v     | List remote repos verbosely
+# git remote show   | Describes a single remote repo
+# git remote update | Fetches the most up-to-date objects
+# git fetch         | Downloads specific objects
+# git branch -r     | Lists remote branches; can be combined with other branch arguments to manage remote branches
+
+
+# Practice Quiz
+# git remote update will fetch the contents of all remote branches and allow us to merge the contents ourselves.
+# If you want to see more information about a particular remote branch, you can use the git remote show command. Don't forget the commit ID!
+# git fetch will download remote updates, such as objects and refs, from the remote branch.
+# git pull automatically merges the remote branch with the current branch.
+# explicit merge
+
+# Solving Conflicts
+#
