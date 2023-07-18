d75c3dbb7d88548083ba3fcf6fc550ca570aef51
Ntt37Vp <fuckspammers@protonmail.com>
2023-07-18 10:12:38 +0800

    Pull requests, Squashing

--- 03-Git/Week-4/notes.py
+++ 03-Git/Week-4/notes.py
@@ -1,6 +1,14 @@
 # Pull Requests
 # Pull Requests for repos that we do not have commit access into 
 
+# after commit, push to remote by using
+# git push -u origin name_of_branch
+
+# Updating an existing Pull Request
+# Git Fork and Pull Requests
+# git push -f (force)
+# pick is the def when using interactive rebase
+
 
 # Code Reviews
 
