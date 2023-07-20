fd97098c394a9b692401284c2bf2bc4265002a3c
Ntt37Vp <fuckspammers@protonmail.com>
2023-07-20 21:47:32 +0800

    Course 4 - Week 1 - init

---
+++ 04-Troubleshooting-Debugging/notes.py
@@ -0,0 +1,9 @@
+# Troubleshooting is the process of identifying, analyzing, and solving problems.
+# Debugging is the process of identifying, analyzing, and removing bugs in a system.
+# Debuggers
+
+# Reproduction case
+
+# Steps:
+# Gather all avail info
+# Finding Root Cause
