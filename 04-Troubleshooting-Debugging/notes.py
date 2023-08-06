8301c92fe7f5383726655016cdd4459962e963a0
Ntt37Vp <fuckspammers@protonmail.com>
2023-08-06 12:55:55 +0800

    more week 3 notes

--- 04-Troubleshooting-Debugging/notes.py
+++ 04-Troubleshooting-Debugging/notes.py
@@ -62,3 +62,24 @@
 
 # A WRAPPER is a function or program that provides a compatibility layer between two functions or programs.
 # A CONTAINER allows the application to run in its own environment without interfering with the rest of the system.
+
+# A PBD file is used to generate debugging symbols using Microsoft compilers.
+# Dr. Memory can assist in finding out if invalid operations are occurring in a program running on Windows or Linux.
+
+# ulimit -c unlimited
+# is used to create a core file that stores information related to a crash.
+
+# gdb
+# gdb command will debug a core dump and stop where the failure was recorded.
+# backtrace -- The backtrace command can be used to show a summary of the function calls that were used to the point where the failure occurs.
+
+# Debugging Python
+# pdb3 filename.py
+# utf8-sig
+
+# QUIZ
+# breakpoints!
+# ram replacement!
+# off-by-one!
+# printf!
+# core files!
