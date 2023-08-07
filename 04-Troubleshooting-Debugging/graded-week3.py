6ac24b26fae16aa1c4a8176e1b1e52d06c8f9ab1
Ntt37Vp <fuckspammers@protonmail.com>
2023-08-07 22:34:53 +0800

    week 3 completed

---
+++ 04-Troubleshooting-Debugging/graded-week3.py
@@ -0,0 +1,22 @@
+# ImportError
+# cd /
+# python3 /usr/bin/infrastructure
+# sudo apt install python3-pip -y
+# pip3 install matplotlib
+
+
+# NoFileError
+# python3 /usr/bin/infrastructure
+# cd ~
+# ls
+# mv data.bak data.csv
+
+# cd /
+# python3 /usr/bin/infrastructure
+
+
+# MissingColumnError
+# cat ~/data.csv
+# sudo chmod 777 ~/data.csv
+# nano ~/data.csv
+# python3 /usr/bin/infrastructure
