e942bb0ead3c1099ad6fb66fc31557b97028a763
Ntt37Vp <fuckspammers@protonmail.com>
2023-08-10 22:17:30 +0800

    more week 1 notes

--- 05-Config-Management-and-the-Cloud/notes.py
+++ 05-Config-Management-and-the-Cloud/notes.py
@@ -1,1 +1,41 @@
 # Week 1 - Automating w/ Config Management
+# Config Management -> Node
+# Puppet (think Ansible ;)
+# IaC (infra as code)
+
+# in Puppet:
+# "Resources" - it's the basic unit for modeling the config
+# "Class" -
+# "Provider" - aka the package manager?
+
+# A class with no parameters
+# class base::linux {
+#   file { '/etc/passwd':
+#     owner => 'root',
+#     group => 'root',
+#     mode  => '0644',
+#   }
+#   file { '/etc/shadow':
+#     owner => 'root',
+#     group => 'root',
+#     mode  => '0440',
+#   }
+# }
+
+# A class with parameters
+# class apache (String $version = 'latest') {
+#   package {'httpd':
+#     ensure => $version, # Using the class parameter from above
+#     before => File['/etc/httpd.conf'],
+#   }
+#   file {'/etc/httpd.conf':
+#     ensure  => file,
+#     owner   => 'httpd',
+#     content => template('apache/httpd.conf.erb'), # Template from a module
+#   }
+#   service {'httpd':
+#     ensure    => running,
+#     enable    => true,
+#     subscribe => File['/etc/httpd.conf'],
+#   }
+# }
