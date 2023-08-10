8c50530841981a66b3ba77a15fd0d2cf014a55ee
Ntt37Vp <fuckspammers@protonmail.com>
2023-08-10 18:33:16 +0800

    week 4 completed

---
+++ 04-Troubleshooting-Debugging/graded-week-4.py
@@ -0,0 +1,23 @@
+# Graded Quiz - Week 4
+
+# Debug issue
+# ls
+# sudo chmod 777 ~/start_date_report.py
+# ./start_date_report.py
+# nano ~/start_date_report.py
+# fix the get_start_date() func by type casting int()
+
+# Improve performance
+# To check the execution time of a script, use the time command
+# ex:
+# time ./start_date_report.py
+# Go to get_same_or_newer() function line
+
+# Here are few hints to fix this issue:
+
+# Download the file only once from the URL.
+
+# Pre-process it so that the same calculation doesn't need to be done over and over again. This can be done in two ways. You can choose any one of them:
+
+# To create a dictionary with the start dates and then use the data in the dictionary instead of the complicated calculation.
+# To sort the data by start_date and then go date by date.
