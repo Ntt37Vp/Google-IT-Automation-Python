612a081b975f953b2333982cf9f43813b91a5e31
Ntt37Vp <fuckspammers@protonmail.com>
2023-08-25 08:42:24 +0800

    week 3 - graded lab - init

---
+++ 06-Real-World-Tasks/graded-week-3.py
@@ -0,0 +1,17 @@
+### Week 3 - Graded Lab - Automatically Generate a PDF and send it by Email ###
+
+## Project Problem Statement
+    # It's that time again, time to flex your programming muscles and practice solving a real-life problem with the skills you acquired!
+
+
+# Sample report
+# In this section, you will be creating a PDF report named "A Complete Inventory of My Fruit". 
+# The script to generate this report and send it by email is already pre-done. You can have a look at the script in the scripts/ directory.
+
+# ls ~/scripts
+
+# In the scripts/ directory, you will find reports.py and emails.py files. These files are used to generate PDF files and send emails respectively.
+
+# Take a look at these files using cat command.
+# cat ~/scripts/reports.py
+# cat ~/scripts/emails.py
--- 06-Real-World-Tasks/notes.py
+++ 06-Real-World-Tasks/notes.py
@@ -73,3 +73,94 @@
 
 
 # Sending using smtplib module
+# built-in email module
+
+# We'll start by using the email.message.EmailMessage class to create an empty email message.
+# in python cli
+# >>> from email.message import EmailMessage
+# >>> message = EmailMessage()
+# >>> print(message)
+# >>> sender = "me@example.com"
+# >>> recipient = "you@example.com"
+# >>> message['From'] = sender
+# >>> message['To'] = recipient
+# >>> print(message)
+# >>> message['Subject'] = 'Greetings from {} to {}!'.format(sender, recipient)
+# >>> print(message)
+# >>> body = """Hey there!
+# ...
+# ... I'm learning to send emails using Python!"""
+# >>> message.set_content(body)
+
+
+# Adding Attachments
+# >>> import os.path
+# >>> import mimetypes
+# >>> attachment_path = "/tmp/example.png"
+# >>> attachment_filename = os.path.basename(attachment_path)
+# >>> mime_type, _ = mimetypes.guess_type(attachment_path)
+# >>> print(mime_type)
+
+
+# >>> mime_type, mime_subtype = mime_type.split('/', 1)
+# >>> print(mime_type)
+# >>> print(mime_subtype)
+
+
+# The SMTPLIB module #
+# Let's create a smtplib.SMTP object and try to connect to the local machine.
+# >>> mail_server = smtplib.SMTP('localhost')
+# >>> mail_server = smtplib.SMTP_SSL('smtp.example.com')
+# mail_server.set_debuglevel(1)
+# >>> import getpass
+# >>> mail_pass = getpass.getpass('Password? ')
+# Password?
+# >>> mail_server.login(sender, mail_pass)
+# (235, b'2.7.0 Accepted')
+
+
+
+### Generating PDFs ###
+
+# using ReportLab
+>>> from reportlab.platypus import SimpleDocTemplate
+>>> report = SimpleDocTemplate("/tmp/report.pdf")
+>>> from reportlab.platypus import Paragraph, Spacer, Table, Image
+>>> from reportlab.lib.styles import getSampleStyleSheet
+>>> styles = getSampleStyleSheet()
+>>> report_title = Paragraph("A Complete Inventory of My Fruit", styles["h1"])
+>>> report.build([report_title])
+
+# adding tables
+>>> table_data = []
+>>> for k, v in fruit.items():
+...   table_data.append([k, v])
+...
+>>> print(table_data)
+[['elderberries', 1], ['figs', 1], ['apples', 2], ['durians', 3], ['bananas', 5], ['cherries', 8], ['grapes', 13]]
+
+>>> report_table = Table(data=table_data)
+>>> report.build([report_title, report_table])
+
+
+# or something like this
+>>> from reportlab.lib import colors
+>>> table_style = [('GRID', (0,0), (-1,-1), 1, colors.black)]
+>>> report_table = Table(data=table_data, style=table_style, hAlign="LEFT")
+>>> report.build([report_title, report_table])
+
+# Adding Graphics to our PDFs
+>>> from reportlab.graphics.shapes import Drawing
+>>> from reportlab.graphics.charts.piecharts import Pie
+>>> report_pie = Pie(width=3*inch, height=3*inch)
+
+>>> report_pie.data = []
+>>> report_pie.labels = []
+>>> for fruit_name in sorted(fruit):
+...   report_pie.data.append(fruit[fruit_name])
+...   report_pie.labels.append(fruit_name)
+...
+>>> print(report_pie.data)
+[2, 5, 8, 3, 1, 1, 13]
+>>> print(report_pie.labels)
+['apples', 'bananas', 'cherries', 'durians', 'elderberries', 'figs', 'grapes']
